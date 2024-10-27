6cb0d2047756d13f1fb370209c95646b5fbb6ee0
codewizrad <codewiizard@gmail.com>
2024-10-27 16:41:04 +0500

    d

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -17,23 +17,21 @@ const Education = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="BSc in Computer Science"
-            subTitle="University of DVI (2006 - 2010)"
+            title="Computer Science"
+            subTitle="MOdevco team (2021 - 2024)"
             result="3.90/4"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."
           />
           <ResumeCard
-            title="AS - Science & Information"
-            subTitle="SuperKing College (2001 - 2005)"
-            result="4.75/5"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
+            title="Front-end Developer"
+            subTitle="MOdevco team (2021 - 2024)"
+            result="3.90/4"
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."          />
           <ResumeCard
-            title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
-            result="5.00/5"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          />
+            title="Back-end Developer"
+            subTitle="MOdevco team (2021 - 2024)"
+            result="3.90/4"
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."          />
         </div>
       </div>
       {/* part Two */}
@@ -44,24 +42,21 @@ const Education = () => {
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <ResumeCard
+            title="Mobile Developer"
+            subTitle="MOdevco team (2021 - 2024)"
+            result="3.90/4"
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."          />
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
-          />
+            title="Fullstack Developer"
+            subTitle="MOdevco team (2021 - 2024)"
+            result="3.90/4"
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."          />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-          />
-          <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
-          />
+            title="Cyber Security"
+            subTitle="MOdevco team (2021 - 2024)"
+            result="3.90/4"
+            des="The training provided by learning center in order to prepare people to work in various sectors of the economy or areas of culture."          />
         </div>
       </div>
     </motion.div>
