e1eb4ed3a8cf3f9eb75dfc77263d5cf21b261516
ZetsuCoder <codewiizard@gmail.com>
2025-01-14 02:19:31 +0500

    m

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -16,31 +16,31 @@ const Features = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <Card
           title="Frontend Development"
-          des="Building responsive and visually appealing user interfaces using modern technologies like React.js, Tailwind CSS, and more."
+          des="Building responsive and visually appealing user interfaces using modern technologies like React.js, Tailwind CSS, and more." m={"-1"}
         />
         <Card
           title="Backend Development"
-          des="Creating robust and scalable backend systems with Node.js, Express.js, and integrating with databases like MongoDB."
+          des="Creating robust and scalable backend systems with Node.js, Express.js, and integrating with databases like MongoDB." m={"-5"}
           icon={<AiFillAppstore />}
         />
         <Card
           title="Mobile App Development"
-          des="Designing and developing cross-platform mobile applications with frameworks like React Native and Flutter."
+          des="Designing and developing cross-platform mobile applications with frameworks like React Native and Flutter." m={"-5"}
           icon={<FaMobile />}
         />
         <Card
           title="UI/UX Design"
-          des="Crafting intuitive and user-friendly interfaces using tools like Figma to enhance user experience."
+          des="Crafting intuitive and user-friendly interfaces using tools like Figma to enhance user experience." m={"0"}
           icon={<SiAntdesign />}
         />
         <Card
           title="Website Hosting"
-          des="Deploying and managing websites on platforms like AWS, Vercel, and Netlify for optimal performance and reliability."
+          des="Deploying and managing websites on platforms like AWS, Vercel, and Netlify for optimal performance and reliability." m={"-1"}
           icon={<FaGlobe />}
         />
         <Card
           title="API Integration"
-          des="Connecting applications with third-party services and custom APIs to streamline functionality and data exchange."
+          des="Connecting applications with third-party services and custom APIs to streamline functionality and data exchange." m={"-5"}
           icon={<TbApi />}
         />
       </div>
